3bb35c02f65cdb4dfbb92ca3be0734e38608e691
denis <dzaimov@npv-23121-imac.carsdirect.win>
2019-02-22 16:11:10 -0500

    added scss and mixins functionality to the project

--- register.php
+++ register.php
@@ -22,73 +22,85 @@ function getInputValue($name) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <div id="inputContainer">
-        <form action="register.php" id="loginForm" method="POST">
-            <h2>Login to your account</h2>
-            <p>
-                <?php echo $account->getError(Constants::$loginFailed) ?>
-                <label for="loginUsername">Username</label>
-                <input type="text" id="loginUsername" name="loginUsername" placeholder="Username" required>
-            </p>
-            <p>
-                <label for="loginPassword">Password</label>
-                <input type="password" id="loginPassword" name="loginPassword" placeholder="Password" required>            
-            </p>
+    <div class="register">
+        <div class="register-wrapper container d-flex justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <form action="register.php" id="loginForm" class="register-wrapper__login-form" method="POST">
+                        <h2>Login to your account</h2>
+                        <p>
+                            <?php echo $account->getError(Constants::$loginFailed) ?>
+                            <label for="loginUsername">Username</label>
+                            <input type="text" id="loginUsername" name="loginUsername" placeholder="Username" required>
+                        </p>
+                        <p>
+                            <label for="loginPassword">Password</label>
+                            <input type="password" id="loginPassword" name="loginPassword" placeholder="Password" required>            
+                        </p>
 
-            <button type="Submit" name="loginButton">Login</button>
-        </form>
+                        <button type="Submit" name="loginButton">Login</button>
+                    </form>
+                </div>
+            </div>
 
-        <form action="register.php" id="registerForm" method="POST">
-            <h2>Create your free account</h2>
-            <p>
-                <?php echo $account->getError(Constants::$usernameLength) ?>
-                <?php echo $account->getError(Constants::$usernameTaken) ?>
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" placeholder="Username" value="<?php getInputValue('username') ?>" required>
-            </p>
+            <div class="row">
+                <div class="col">
+                <form action="register.php" id="registerForm" class="register-container__register-form" method="POST">
+                    <h2>Create your free account</h2>
+                    <p>
+                        <?php echo $account->getError(Constants::$usernameLength) ?>
+                        <?php echo $account->getError(Constants::$usernameTaken) ?>
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="username" placeholder="Username" value="<?php getInputValue('username') ?>" required>
+                    </p>
 
-            <p>
-                <?php echo $account->getError(Constants::$firstNameLength) ?>
-                <label for="firstName">First Name</label>
-                <input type="text" id="firstName" name="firstName" placeholder="First Name" value="<?php getInputValue('firstName') ?>" required>
-            </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$firstNameLength) ?>
+                        <label for="firstName">First Name</label>
+                        <input type="text" id="firstName" name="firstName" placeholder="First Name" value="<?php getInputValue('firstName') ?>" required>
+                    </p>
 
-            <p>
-                <?php echo $account->getError(Constants::$lastNameLength) ?>
-                <label for="lastName">Last Name</label>
-                <input type="text" id="lastName" name="lastName" placeholder="Last Name" value="<?php getInputValue('lastName') ?>" required>
-            </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$lastNameLength) ?>
+                        <label for="lastName">Last Name</label>
+                        <input type="text" id="lastName" name="lastName" placeholder="Last Name" value="<?php getInputValue('lastName') ?>" required>
+                    </p>
 
-            <p>
-                <?php echo $account->getError(Constants::$emailsDoNotMatch) ?> 
-                <?php echo $account->getError(Constants::$emailInvalid) ?>
-                <?php echo $account->getError(Constants::$emailTaken) ?>
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Email" value="<?php getInputValue('email') ?>" required>
-            </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$emailsDoNotMatch) ?> 
+                        <?php echo $account->getError(Constants::$emailInvalid) ?>
+                        <?php echo $account->getError(Constants::$emailTaken) ?>
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Email" value="<?php getInputValue('email') ?>" required>
+                    </p>
 
-            <p>
-                <label for="email2">Confirm Email</label>
-                <input type="email" id="email2" name="email2" placeholder="Email" value="<?php getInputValue('email2') ?>" required>
-            </p>
+                    <p>
+                        <label for="email2">Confirm Email</label>
+                        <input type="email" id="email2" name="email2" placeholder="Email" value="<?php getInputValue('email2') ?>" required>
+                    </p>
 
-            <p>
-                <?php echo $account->getError(Constants::$passwordsDoNotMatch) ?> 
-                <?php echo $account->getError(Constants::$passwordNotAlphanumeric) ?>
-                <?php echo $account->getError(Constants::$passwordLength) ?>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Password" required>            
-            </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$passwordsDoNotMatch) ?> 
+                        <?php echo $account->getError(Constants::$passwordNotAlphanumeric) ?>
+                        <?php echo $account->getError(Constants::$passwordLength) ?>
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password" required>            
+                    </p>
 
-            <p>
-                <label for="password2">Confirm Password</label>
-                <input type="password" id="password2" name="password2" placeholder="Password" required>            
-            </p>
+                    <p>
+                        <label for="password2">Confirm Password</label>
+                        <input type="password" id="password2" name="password2" placeholder="Password" required>            
+                    </p>
 
-            <button type="Submit" name="registerButton">Sign Up</button>
-        </form>
+                    <button type="Submit" name="registerButton">Sign Up</button>
+                </form>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
