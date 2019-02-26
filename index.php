73f4ce0257de55620a3a0d7ecb42331cf0ecae0a
denis <dzaimov@npv-23121-imac.carsdirect.win>
2019-02-26 16:26:31 -0500

    finished sidebar styles

--- index.php
+++ index.php
@@ -22,6 +22,30 @@ if(isset($_SESSION['userLoggedIn'])) {
 </head>
 <body>
     <div class="media">
+
+        <div class="media__content">
+            <div class="media__content-sidebar">
+                <nav class="media__content-navbar">
+                    <a href="index.php" class="media__content-logo">
+                        <img src="assets/images/icons/logo.jpeg" alt="logo">
+                    </a>
+
+                    <div class="media__content-items">
+                        <a href="search.php" class="media__content-search">Search
+                            <img src="assets/images/icons/search.png" alt="search icon" class="search-icon">
+                        </a>
+                        
+                    </div>
+
+                    <div class="media__content-items">
+                        <a href="browse.php">Browse</a>
+                        <a href="library.php">Your Library</a>
+                        <a href="profile.php">User Profile</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
         <div class="media__player">
             <div class="media__playerbar">
                 <div class="media__playerbar-left">
