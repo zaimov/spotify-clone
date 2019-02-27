b994daf114fb3f3e3683b99f366d3f5b92d5244d
denis <denis.zaimov@martindale.com>
2019-02-27 15:34:20 -0500

    changes to db and settings

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@
 
     $timezone = date_default_timezone_set("America/New_York");
 
-    $con = mysqli_connect("localhost", "root", "root", "spotify-clone");
+    $con = mysqli_connect("mariadb", "root", "root", "spotify-clone");
 
     if (mysqli_connect_errno()) {
         echo "Failed to connect: " . mysqli_connect_errno();
